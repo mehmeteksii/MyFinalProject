3447008da67bf2ffed0a519a355cd7042f33670c
mehmeteksii <eksimehmet02@gmail.com>
2022-10-25 00:32:31 +0300

    13. Ders Sonu

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -10,11 +10,12 @@ using System.Threading.Tasks;
 
 namespace Core.Aspects.Autofac.Validation
 {
-    public class ValidationAspect : MethodInterception
+    public class ValidationAspect : MethodInterception //Aspect == methodun neresinde istersen (başında ortasında sonunda hatada) çalışan yapı
     {
         private Type _validatorType;
         public ValidationAspect(Type validatorType)
         {
+            //defensive coding
             if (!typeof(IValidator).IsAssignableFrom(validatorType))
             {
                 throw new System.Exception("Bu bir doğrulama sınıfı değil");
@@ -24,7 +25,7 @@ namespace Core.Aspects.Autofac.Validation
         }
         protected override void OnBefore(IInvocation invocation)
         {
-            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);//product validator burada new lendi
             var entityType = _validatorType.BaseType.GetGenericArguments()[0];
             var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
             foreach (var entity in entities)
